22a66869f256ea3c7e1380b0ba6db9276cb4a7a0
yusuke <mopu0920@gmail.com>
2020-11-24 15:29:17 +0900

    test

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    /* ---------------------------------
+
+    ハンバーガーメニュー
+
+    --------------------------------- */
+    $("#toggle").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(".toggle-menu").addClass("active");
+        } else {
+            $(".toggle-menu").removeClass("active");
+        }
+    });
+
+    $(".toggle-menu__link").click(function() {
+        $(".toggle-menu, .toggle").removeClass("active");
+    });
