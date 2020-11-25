9d2cdae7a015c9caba75fc1a6e911cb56bbf3716
yusuke <mopu0920@gmail.com>
2020-11-25 15:13:58 +0900

    test

--- js/script.js
+++ js/script.js
@@ -19,32 +19,3 @@ $(function() {
         $(".toggle-menu, .toggle").removeClass("active");
     });
 
-
-    /* ---------------------------------
-
-    スマホでのクリック対応
-
-    --------------------------------- */
-$(function () {
-    // hover
-    var userAgent = navigator.userAgent;
-    var item = $("a").add("button");
-
-    if (userAgent.indexOf("iPhone") >= 0 || userAgent.indexOf("iPad") >= 0 || userAgent.indexOf("Android") >= 0) {
-        item.on("touchstart", function () {
-            $(this).addClass("hover");
-        });
-        item.on("touchend", function () {
-            $(this).removeClass("hover");
-        });
-    } else {
-        item.hover(
-            function () {
-                $(this).addClass("hover");
-            },
-            function () {
-                $(this).removeClass("hover");
-            }
-        );
-    }
-});
